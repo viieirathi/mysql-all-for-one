ed05ccf6d6f821b1f8bab1b8307b782e4587a47c
Thiago <thiivieira@gmail.com>
2022-01-31 20:23:24 -0300

    Arrumando Requisito 20

--- desafio20.sql
+++ desafio20.sql
@@ -1,3 +1,4 @@
 INSERT INTO order_detais 
     (order_id, product_id, quantity,unit_price, discount, status_id,date_allocated,purchase_order_id, inventory_id)
-VALUES (69, 80,15.0000, 0, 2, null, null, 129);
+VALUES 
+    (69, 80,15.0000, 0, 2, null, null, 129);
